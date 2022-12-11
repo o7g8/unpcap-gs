60740dcb4724931b3d5ba3b0faab94bcf03331b3
Oleg Grytsynevych <oleggry@amazon.com>
2022-12-12 00:31:14 +0100

    Reading file header.

--- PcapReader.cs
+++ PcapReader.cs
@@ -5,23 +5,46 @@ namespace unpcap;
 
 public class PcapReader : IEnumerable<PcapRecord>
 {
+    private MagicNumber byteOrder;
+    private System.UInt32 capturedBytes;
     private readonly Stream input;
-    private readonly byte[] buffer = new byte[64 * 1024 + 300]; // max UDP ptk size + 300 bytes for headers
+    private bool noData = false;
 
     public PcapReader(Stream input)
     {
         this.input = input;
-        var header = ReadFileHeader(input, buffer);
+        var (eof, header) = ReadFileHeader(input);
+        if(eof) {
+            noData = true;
+        } else {
+            byteOrder = header.MagicNumber;
+            capturedBytes = header.SnapLen;
+        }
     }
 
     public IEnumerator<PcapRecord> GetEnumerator()
     {
-        throw new NotImplementedException();
+        while (true)
+        {
+            var (eof, recordHeader) = ReadRecordHeader(input);
+            if(eof) {
+                yield break;
+            }
+
+            var packetLength = (int)(recordHeader.InclLen);
+            var buffer = new byte[packetLength];
+
+            var bytesRead = ReadFull(input, buffer, packetLength);
+            var record = bytesRead < packetLength ?
+                buffer[0..bytesRead]
+                : buffer;
+            yield return new PcapRecord(recordHeader, record);
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        throw new NotImplementedException();
+        return this.GetEnumerator();
     }
 
     // https://www.codeproject.com/Articles/5296628/Fast-Conversions-between-tightly-packed-Structures
@@ -41,22 +64,31 @@ public class PcapReader : IEnumerable<PcapRecord>
     }
 
     # region Blocking
-    private PcapRecordHeader ReadRecordHeader(Stream stream, byte[] buffer)
+    // returns (eof, data)
+    private (bool, PcapRecordHeader) ReadRecordHeader(Stream stream)
     {
+        var buffer = new byte[Constants.PcapRecordHeader_Length];
         var length = ReadFull(input, buffer, Constants.PcapRecordHeader_Length);
+        if(length == 0) {
+            return (true, new PcapRecordHeader());
+        }
         if(length != Constants.PcapRecordHeader_Length) {
             throw new Exception("EOF: too short PCAP record header.");
         }
-        return ArrayToStructure<PcapRecordHeader>(buffer);
+        return (false, ArrayToStructure<PcapRecordHeader>(buffer));
     }
 
-    private PcapFileHeader ReadFileHeader(Stream stream, byte[] buffer)
+    private (bool, PcapFileHeader) ReadFileHeader(Stream stream)
     {
+        var buffer = new byte[Constants.PcapFileHeader_Length];
         var length = ReadFull(input, buffer, Constants.PcapFileHeader_Length);
+        if(length == 0) {
+            return (true, new PcapFileHeader());
+        }
         if(length != Constants.PcapFileHeader_Length) {
             throw new Exception("EOF: too short PCAP file header.");
         }
-        return ArrayToStructure<PcapFileHeader>(buffer);
+        return (false, ArrayToStructure<PcapFileHeader>(buffer));
     }
 
     private int ReadFull(Stream stream, byte[] buffer, int length)
